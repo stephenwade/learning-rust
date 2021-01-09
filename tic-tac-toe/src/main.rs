0b55cdc58e790ea5b62fd74281d84dd0d5764739
joshuawade <wadej@southern.edu>
2021-01-09 15:25:52 -0500

    Add Board and Game structs

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+enum BoardValue {
+    None,
+    X,
+    O,
+}
+
+impl Default for BoardValue {
+    fn default() -> Self {
+        BoardValue::None
+    }
+}
+
+#[derive(Debug, Default)]
+struct Board([[BoardValue; 3]; 3]);
+
+struct Game {
+    board: Board,
+}
+
+impl Game {
+    fn new() -> Self {
+        Game {
+            board: Board::default(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let game = Game::new();
+    println!("{:?}", game.board);
 }
