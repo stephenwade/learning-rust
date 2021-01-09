a8b2c0a26f0fef837fd5e5b54eb79796bb578ba4
joshuawade <wadej@southern.edu>
2021-01-09 17:37:36 -0500

    Add input parsing code

--- tic-tac-toe/src/main.rs
+++ tic-tac-toe/src/main.rs
@@ -1,4 +1,7 @@
 use std::fmt;
+use std::str;
+
+use rustyline::Editor;
 
 enum BoardValue {
     None,
@@ -26,6 +29,30 @@ impl Default for BoardValue {
     }
 }
 
+enum Player {
+    X,
+    O,
+}
+
+impl fmt::Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Player::X => "X",
+                Player::O => "O",
+            }
+        )
+    }
+}
+
+impl Default for Player {
+    fn default() -> Self {
+        Player::X
+    }
+}
+
 #[derive(Default)]
 struct Board {
     state: [[BoardValue; 3]; 3],
@@ -46,21 +73,73 @@ impl fmt::Display for Board {
 
 struct Game {
     board: Board,
+    current_player: Player,
 }
 
 impl Game {
     fn new() -> Self {
         Game {
             board: Board::default(),
+            current_player: Player::default(),
         }
     }
 }
 
 fn main() {
     let game = Game::new();
+
     println!("Welcome to tic tac toe!");
     loop {
         println!("{}", game.board);
+        println!("It's {}'s turn.", game.current_player);
+        let inputs = get_row_and_column().unwrap();
+        println!("{}, {}", inputs.0, inputs.1);
         break;
     }
+}
+
+fn get_row_and_column() -> Result<(usize, usize), ()> {
+    let strings = match read_row_and_column_strings() {
+        Ok(strings) => strings,
+        Err(_) => return Err(()),
+    };
+
+    let numbers = match parse_row_and_column_strings(strings) {
+        Ok(numbers) => numbers,
+        Err(_) => return Err(()),
+    };
+
+    Ok((numbers.0 - 1, numbers.1 - 1))
+}
+
+fn parse_row_and_column_strings(
+    input: (String, String),
+) -> Result<(usize, usize), <usize as str::FromStr>::Err> {
+    let row: usize = input.0.parse()?;
+    let column: usize = input.1.parse()?;
+
+    Ok((row, column))
+}
+
+fn read_row_and_column_strings() -> Result<(String, String), rustyline::error::ReadlineError> {
+    let mut rl = Editor::<()>::new();
+
+    let valid_inputs: Vec<&str> = vec!["1", "2", "3"];
+    let mut row_line: String;
+    let mut column_line: String;
+
+    loop {
+        row_line = rl.readline("Enter a row (1, 2, 3): ")?;
+        if valid_inputs.iter().any(|&s| row_line == s) {
+            break;
+        };
+    }
+    loop {
+        column_line = rl.readline("Enter a column (1, 2, 3): ")?;
+        if valid_inputs.iter().any(|&s| column_line == s) {
+            break;
+        };
+    }
+
+    Ok((row_line, column_line))
 }
